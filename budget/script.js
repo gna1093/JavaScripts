775af34e8ad27a8047b6bf9e9b91d48126f8103b
gp <gnanaprakasam18@gmail.com>
2020-05-07 13:04:53 +0530

    budget project

---
+++ budget/script.js
@@ -0,0 +1,31 @@
+//budget controller
+var budgetController = (function(){
+
+  //some code
+
+})();
+
+
+//UI controller
+var UIController = (function(){
+    //some code
+
+})();
+
+var Controller = (function(budgetCtrl,UICtrl){
+
+    document.querySelector('.add__btn').addEventListener('click',function(){
+      console.log("clicked");
+
+      //1. get the filed input data
+
+      //2. add item to the budget__title
+
+      //3. add item to const util = require('util');
+
+      //4. calcualte the budget__title
+
+      //
+    });
+
+})(budgetController,UIController);
