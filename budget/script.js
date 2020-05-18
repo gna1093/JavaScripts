3234198137efbfa6ca4d035104ca1b6766982e31
gp <gnanaprakasam18@gmail.com>
2020-05-18 13:29:55 +0530

    UI controller

--- budget/script.js
+++ budget/script.js
@@ -8,24 +8,51 @@ var budgetController = (function(){
 
 //UI controller
 var UIController = (function(){
-    //some code
+
+  var DOMstrings = {
+    inputType : '.add_type',
+    inputDescription : '.add__description',
+    inputValue : '.add__value'
+  }
+
+  return {
+    getinput : function(){
+
+      return {
+        type : document.querySelector(DOMstrings.inputType).value, //will get income or expense
+        description : document.querySelector(DOMstrings.inputDescription).value,
+        value : document.querySelector(DOMstrings.inputType).value
+            }
+    }
+  }
 
 })();
 
 var Controller = (function(budgetCtrl,UICtrl){
 
-    document.querySelector('.add__btn').addEventListener('click',function(){
-      console.log("clicked");
 
-      //1. get the filed input data
+
+    var ctrlAddItem = function(){
+      var input = UICtrl.getinput();
+      console.log(input);
 
       //2. add item to the budget__title
 
-      //3. add item to const util = require('util');
+      //3. add item to the UI
 
       //4. calcualte the budget__title
 
-      //
+      //5. Display
+      console.log('Its works');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress',function(event){
+        if(event.keyCode === 13 || event.which ===13){
+            console.log('ENTER pressed');
+            ctrlAddItem();
+        }
+
     });
 
 })(budgetController,UIController);
