be329934849880a41caa8bcd61d8c80f69be7e9e
gp <gnanaprakasam18@gmail.com>
2020-06-08 11:32:16 +0530

    first api call

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,13 @@
-// Global app controller
-import x from './test';
-const y =23;
-console.log(`Imported ${x} from test.js and ${y}`);
+import Search from './modules/Search';
+
+/* Global stae of the app
+search object
+current obecr receipe
+shopping list object
+liked recipes
+*/
+const state = {}
+
+const search = new Search('pizza');
+console.log(search);
+search.getResult()
